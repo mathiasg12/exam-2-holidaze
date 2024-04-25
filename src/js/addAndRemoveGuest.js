8f298a535cbb0f0373b0de2d2aa2be0b777fa40a
mathiasg12 <mathiasgausl@gmail.com>
2024-04-25 14:27:44 +0200

    created a function to book venues, added validation to datePicker inputs and when a user book a venue, they will eighter recive a error message or a success overlay.

---
+++ src/js/addAndRemoveGuest.js
@@ -0,0 +1,29 @@
+/**
+ *  Two functions that handle the addition and removal of guests on the specific site.
+ *  These functions allow users to either add or remove the number of guests traveling.
+ *  Users cannot add more guests than the allowed limit returned from the venue,
+ *  and they cannot add fewer guests than one.
+ * @param {state} guestValue
+ * @param {state} setGuestValue
+ * @param {state} venue
+ * @param {const} label
+ */
+export function handleRemoveGuest(guestValue, setGuestValue, venue, label) {
+  label.innerText = `How many are staying? (min 1 and max ${venue.maxGuests})`;
+  if (guestValue - 1 <= 0) {
+    label.innerText = `Atleast 1 guest`;
+    setGuestValue(1);
+  } else {
+    setGuestValue(guestValue - 1);
+  }
+}
+export function handleAddGuest(guestValue, setGuestValue, venue, label) {
+  const maxGuestFloat = parseFloat(venue.maxGuests);
+  label.innerText = `How many are staying? (min 1 and max ${venue.maxGuests})`;
+  if (guestValue + 1 > maxGuestFloat) {
+    setGuestValue(maxGuestFloat);
+    label.innerText = `Max ${venue.maxGuests} guests`;
+  } else {
+    setGuestValue(guestValue + 1);
+  }
+}
